f1607c94cafe7c4fe8e320f0c8ca7d460d9d6d17
TIAGO AGUSTIN GHELMAN <48245701@ort>
2025-05-09 10:38:23 -0300

    sjjdsj

--- APP/App.js
+++ APP/App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView,TextInput,ImageBackground,Button,TouchableOpacity,Pressable,flex} from 'react-native';
 
+const imagenTiago = requiere("./assets/Tiago.PNG")
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <StatusBar style="dark" />
+
+      <ImageBackground
+        source = {imagenTiago} 
+        resizeMode='cover'
+        style={styles.ImageBackground}>
+      </ImageBackground>
+
+      <Text style={styles.titulo}>Buen ser humano, mejor robot</Text>
+      <Text style={styles.nombre}>Tiago Ghelman</Text>
+
+    <TextInput
+    style={styles.inputtext}
+    placeholder='Ingrese un comentario'
+    >
+      
+    </TextInput>
+
     </View>
   );
 }
@@ -16,5 +34,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo:
+  {
+    fontSize : 20 , 
+    color : '#354666'
+  },
+  nombre:
+  {
+    fontSize : 16 , 
+    color : '#FFA500'
   },
 });
